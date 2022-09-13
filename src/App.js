b90312cc9dbb3b351247a8167d08f0fae2b7a7bf
djordjemojsic <djordjemojsic2001@gmail.com>
2022-09-13 23:39:19 +0200

    uradili rutu za prijavu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import LandingPage from "./Components/LandingPage/Landing";
-import AboutFonis from "./Components/AboutFonis/AboutFonis";
-import Hexagon from "./Components/HexagonOpsti/Hexagon";
-import Radionice from "./Components/Radionice/Radionice";
-import Agenda from "./Components/Agenda/Agenda";
-import OrgTim3 from "./Components/OrgTim3/OrgTim3";
-import "./fonts/Jost500Medium.otf";
-import "./fonts/Jost700Bold.otf";
-import Panelisti from "./Components/Panelisti/Panelisti";
-import Footer from "./Components/Footer/Footer";
-import Partneri from "./Components/Partneri/Partneri";
+import Home from "./Home";
+import Prijava from "./Components/Prijava/Prijava";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-      <AboutFonis />
-      <Panelisti />
-      <Agenda />
-      <Partneri />
-      <OrgTim3 />
-      <Footer />
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/prijava" exact element={<Prijava />} />
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/Components/Prijava/Prijava.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Prijava = () => {
+  return (
+    <div>
+      <h1 className="PRIJAVA">POZ</h1>
+    </div>
+  );
+};
+
+export default Prijava;
