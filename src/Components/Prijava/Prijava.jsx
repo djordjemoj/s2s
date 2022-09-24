4fcbdd271be910bd7bf56b3fc2266b5f91bcb18d
djordjemojsic <djordjemojsic2001@gmail.com>
2022-09-24 22:06:22 +0200

    gotova forma prijava top

--- src/Components/Prijava/Prijava.jsx
+++ src/Components/Prijava/Prijava.jsx
@@ -8,6 +8,14 @@ import CustomSelect from "./CustomSelect";
 import CustomTextArea from "./CustomTextArea";
 import axios from "axios";
 import "./Prijava.css";
+import {
+  LazyLoadImage,
+  trackWindowScroll,
+} from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import S2SLOGO from "../../assets/s2slogo.png";
+// import logoplaceholder from "../../assets/smallLogo.png";
+
 // import { json } from "express";
 
 // await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -161,93 +169,116 @@ const Prijava = () => {
 
   return (
     <Scrollbars style={{ height: "100vh" }}>
-      <Formik
-        initialValues={{
-          imePrezime: "",
-          emailPriv: "",
-          brojTelefona: "",
-          fakultet: "",
-          godinaStudija: "",
-          newsletter: false,
-          daLiJeRanijeUcestvovao: "",
-          generalnaMotivacija: "",
-          panelDaLi: false,
-          radionicaDaLi: false,
-          pitanjaPanelistima: "",
-          prvaRadionica: "",
-          prvaMotivacija: "",
-          drugaRadionica: "",
-          drugaMotivacija: "",
-        }}
-        validationSchema={advancedSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div className="imePrezimeDiv">
-              <label className="brojPitanja">1.</label>
-              <CustomInput
-                label="Ime i prezime"
-                name="imePrezime"
-                type="text"
-                placeholder="Unesite Vaše ime i prezime"
-              />
+      <div className="Prijava-ceo">
+        <div className="PrijavaTop">
+          <a className="PrijavaTop-logo" href="#">
+            <LazyLoadImage
+              alt="s2sLogo"
+              src={S2SLOGO}
+              // width="100%"
+              height="100%"
+              className="prijLogo"
+              effect="blur"
+              // placeholder={"<span>Loading..</span>"}
+              // placeholderSrc={logoplaceholder}
+            />
+            {/* <img src={S2SLOGO} alt="ss" width="200px"
+            height="200px" /> */}
+          </a>
+          <div className="PrijavaTop-svg">
+            <div className="UmestoSVG">
+              <h1>PRIJAVA</h1>
             </div>
-            <div className="emailDiv">
-              <label className="brojPitanja">2.</label>
-              <CustomInput
-                label="Email adresa"
-                name="emailPriv"
-                type="email"
-                placeholder="Unesite Vašu email adresu"
-              />
-            </div>
-            <div className="telefonDiv">
-              <label className="brojPitanja">3.</label>
-              <CustomInput
-                label="Broj telefona"
-                name="brojTelefona"
-                type="number"
-                placeholder="Unesite Vaš broj telefona"
-              />
-            </div>
-            <div className="fakultetDiv">
-              <label className="brojPitanja">4.</label>
-              <CustomSelect
-                label="Fakultet"
-                name="fakultet"
-                placeholder="Izaberite koji fakultet studirate"
-              >
-                <option value="">Izaberite koji fakultet studirate</option>
-                <option value="fon">FON</option>
-                <option value="etf">ETF</option>
-                <option value="matf">MATF</option>
-                <option value="raf">RAF</option>
-                <option value="viser">VIŠER</option>
-                <option value="ict">ICT</option>
-                <option value="drugo">Drugo</option>
-              </CustomSelect>
-            </div>
-            <div className="godinaDiv">
-              <label className="brojPitanja">5.</label>
-              <CustomSelect
-                label="Godina studija"
-                name="godinaStudija"
-                placeholder="Izaberite Vašu godinu studija"
-              >
-                <option value="">Izaberite Vašu godinu studija</option>
-                <option value="prva">Prva</option>
-                <option value="druga">Druga</option>
-                <option value="treca">Treća</option>
-                <option value="cetvrta">Četvrta</option>
-                <option value="peta">Peta</option>
-                <option value="master">Master</option>
-                <option value="doktorat">Doktorat</option>
-              </CustomSelect>
-            </div>
-            <div className="newsletterDiv">
-              <label className="brojPitanja">6.</label>
-              {/* <CustomSelect
+          </div>
+        </div>
+        <Formik
+          className="FormaPrijava"
+          initialValues={{
+            imePrezime: "",
+            emailPriv: "",
+            brojTelefona: "",
+            fakultet: "",
+            godinaStudija: "",
+            newsletter: false,
+            daLiJeRanijeUcestvovao: "",
+            generalnaMotivacija: "",
+            panelDaLi: false,
+            radionicaDaLi: false,
+            pitanjaPanelistima: "",
+            prvaRadionica: "",
+            prvaMotivacija: "",
+            drugaRadionica: "",
+            drugaMotivacija: "",
+          }}
+          validationSchema={advancedSchema}
+          onSubmit={onSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className="imePrezimeDiv">
+                <label className="brojPitanja">1.</label>
+                <CustomInput
+                  label="Ime i prezime"
+                  name="imePrezime"
+                  type="text"
+                  placeholder="Unesite Vaše ime i prezime"
+                />
+              </div>
+              <div className="emailDiv">
+                <label className="brojPitanja">2.</label>
+                <CustomInput
+                  label="Email adresa"
+                  name="emailPriv"
+                  type="email"
+                  placeholder="Unesite Vašu email adresu"
+                />
+              </div>
+              <div className="telefonDiv">
+                <label className="brojPitanja">3.</label>
+                <CustomInput
+                  label="Broj telefona"
+                  name="brojTelefona"
+                  type="number"
+                  placeholder="Unesite Vaš broj telefona"
+                />
+              </div>
+              <div className="fakultetDiv">
+                <label className="brojPitanja">4.</label>
+                <CustomSelect
+                  label="Fakultet"
+                  name="fakultet"
+                  placeholder="Izaberite koji fakultet studirate"
+                >
+                  <option value="">Izaberite koji fakultet studirate</option>
+                  <option value="fon">FON</option>
+                  <option value="etf">ETF</option>
+                  <option value="matf">MATF</option>
+                  <option value="raf">RAF</option>
+                  <option value="viser">VIŠER</option>
+                  <option value="ict">ICT</option>
+                  <option value="drugo">Drugo</option>
+                </CustomSelect>
+              </div>
+              <div className="godinaDiv">
+                <label className="brojPitanja">5.</label>
+                <CustomSelect
+                  label="Godina studija"
+                  name="godinaStudija"
+                  placeholder="Izaberite Vašu godinu studija"
+                >
+                  <option value="">Izaberite Vašu godinu studija</option>
+                  <option value="prva">Prva</option>
+                  <option value="druga">Druga</option>
+                  <option value="treca">Treća</option>
+                  <option value="cetvrta">Četvrta</option>
+                  <option value="peta">Peta</option>
+                  <option value="master">Master</option>
+                  <option value="doktorat">Doktorat</option>
+                </CustomSelect>
+              </div>
+              <div className="newsletterDiv">
+                <label className="brojPitanja">6.</label>
+                {/* <CustomSelect
                 label="Da li želiš da dobijaš obaveštenja o drugim FONIS projektima i aktivnostima "
                 name="newsletter"
                 placeholder=""
@@ -259,15 +290,15 @@ const Prijava = () => {
                 <option value= {true}>Da</option>
                 <option value={false}>Ne</option>
               </CustomSelect> */}
-              <CustomCheckbox
-                label="Zelim da dobijam obaveštenja o drugim FONIS projektima i aktivnostima "
-                type="checkbox"
-                name="newsletter"
-              />
-            </div>
-            <div className="prethodnoDiv">
-              <label className="brojPitanja">7.</label>
-              {/* <CustomSelect
+                <CustomCheckbox
+                  label="Zelim da dobijam obaveštenja o drugim FONIS projektima i aktivnostima "
+                  type="checkbox"
+                  name="newsletter"
+                />
+              </div>
+              <div className="prethodnoDiv">
+                <label className="brojPitanja">7.</label>
+                {/* <CustomSelect
                 label="Da li si ranije učestvovao/la na S2S-u? "
                 name="daLiJeRanijeUcestvovao"
                 placeholder="Da li si ranije učestvovao/la na S2S-u? "
@@ -278,99 +309,99 @@ const Prijava = () => {
                 <option value={true}>Da</option>
                 <option value={true}>Ne</option>
               </CustomSelect> */}
-              <CustomCheckbox
-                label="Da li si ranije učestvovao/la na S2S-u?"
-                type="checkbox"
-                name="daLiJeRanijeUcestvovao"
-              />
-            </div>
-            <div className="generalnaMotivacijaDiv">
-              <label className="brojPitanja">8.</label>
-              <CustomTextArea
-                label="Šta te je navelo da se ove godine prijaviš na S2S? "
-                name="generalnaMotivacija"
-                placeholder="Objasni motivaciju za projekat xxxxx"
-              ></CustomTextArea>
-            </div>
-            <div className="panelDiv">
-              <label className="brojPitanja">9.</label>
-              <CustomCheckbox
-                label="Želim da se prijavim za panel diskusiju"
-                type="checkbox"
-                name="panelDaLi"
-              />
-            </div>
-            <div className="radionicaDiv">
-              <label className="brojPitanja">10.</label>
-              <CustomCheckbox
-                label="Želim da se prijavim za radionicu"
-                type="checkbox"
-                name="radionicaDaLi"
-              />
-            </div>
-            <div className="pitanjaPanelistima">
-              <label className="brojPitanja">11.</label>
-              <CustomTextArea
-                label="Ovogodišnja tema panela je xxx. Koja pitanja bi postavio/la panelistima na ovu temu? "
-                name="pitanjaPanelistima"
-                placeholder="Ovogodišnja tema panela je xxx. Koja pitanja bi postavio/la panelistima na ovu temu? "
-              ></CustomTextArea>
-            </div>
-            <div className="prviIzborDiv">
-              <label className="brojPitanja">12.</label>
-              <CustomSelect
-                label="Odaberi radionicu na kojoj primarno želiš da prisustvuješ"
-                name="prvaRadionica"
-                placeholder="Odaberi radionicu na kojoj primarno želiš da prisustvuješ"
-              >
-                <option value="">
-                  Odaberi radionicu na kojoj primarno želiš da prisustvuješ
-                </option>
-                <option value="java">Java</option>
-                <option value="python">Python</option>
-                <option value="react">React</option>
-                <option value="figma">Figma</option>
-              </CustomSelect>
-            </div>
-            <div className="prvaMotivacijaDiv">
-              <label className="brojPitanja">13.</label>
-              <CustomTextArea
-                label="Šta te najviše interesuje u vezi oblasti koja se prelazi na ovoj radionici? "
-                name="prvaMotivacija"
-                placeholder="Unesi motivaciju za radionicu "
-              ></CustomTextArea>
-            </div>
-            <div className="drugiIzborDiv">
-              <label className="brojPitanja">14.</label>
-              <CustomSelect
-                label="Odaberi alternativnu radionicu kojoj želiš da prisustvuješ"
-                name="drugaRadionica"
-                placeholder="sta je ovo"
-              >
-                <option value="">
-                  Odaberi alternativnu radionicu kojoj želiš da prisustvuješ
-                </option>
-                <option value="Java">Java</option>
-                <option value="Python">Python</option>
-                <option value="React">React</option>
-                <option value="Figma">Figma</option>
-              </CustomSelect>
-            </div>
-            <div className="drugaMotivacija">
-              <label className="brojPitanja">15.</label>
-              <CustomTextArea
-                label="Šta te najviše interesuje u vezi oblasti koja se prelazi na ovoj radionici? "
-                name="drugaMotivacija"
-                placeholder="Zasto se prijavljujes za ovo "
-              ></CustomTextArea>
-            </div>
-            <button disabled={isSubmitting} type="submit">
-              Prijava
-            </button>
-          </Form>
-        )}
-      </Formik>
-      {/* {modalOpen ? (
+                <CustomCheckbox
+                  label="Da li si ranije učestvovao/la na S2S-u?"
+                  type="checkbox"
+                  name="daLiJeRanijeUcestvovao"
+                />
+              </div>
+              <div className="generalnaMotivacijaDiv">
+                <label className="brojPitanja">8.</label>
+                <CustomTextArea
+                  label="Šta te je navelo da se ove godine prijaviš na S2S? "
+                  name="generalnaMotivacija"
+                  placeholder="Objasni motivaciju za projekat xxxxx"
+                ></CustomTextArea>
+              </div>
+              <div className="panelDiv">
+                <label className="brojPitanja">9.</label>
+                <CustomCheckbox
+                  label="Želim da se prijavim za panel diskusiju"
+                  type="checkbox"
+                  name="panelDaLi"
+                />
+              </div>
+              <div className="radionicaDiv">
+                <label className="brojPitanja">10.</label>
+                <CustomCheckbox
+                  label="Želim da se prijavim za radionicu"
+                  type="checkbox"
+                  name="radionicaDaLi"
+                />
+              </div>
+              <div className="pitanjaPanelistima">
+                <label className="brojPitanja">11.</label>
+                <CustomTextArea
+                  label="Ovogodišnja tema panela je xxx. Koja pitanja bi postavio/la panelistima na ovu temu? "
+                  name="pitanjaPanelistima"
+                  placeholder="Ovogodišnja tema panela je xxx. Koja pitanja bi postavio/la panelistima na ovu temu? "
+                ></CustomTextArea>
+              </div>
+              <div className="prviIzborDiv">
+                <label className="brojPitanja">12.</label>
+                <CustomSelect
+                  label="Odaberi radionicu na kojoj primarno želiš da prisustvuješ"
+                  name="prvaRadionica"
+                  placeholder="Odaberi radionicu na kojoj primarno želiš da prisustvuješ"
+                >
+                  <option value="">
+                    Odaberi radionicu na kojoj primarno želiš da prisustvuješ
+                  </option>
+                  <option value="java">Java</option>
+                  <option value="python">Python</option>
+                  <option value="react">React</option>
+                  <option value="figma">Figma</option>
+                </CustomSelect>
+              </div>
+              <div className="prvaMotivacijaDiv">
+                <label className="brojPitanja">13.</label>
+                <CustomTextArea
+                  label="Šta te najviše interesuje u vezi oblasti koja se prelazi na ovoj radionici? "
+                  name="prvaMotivacija"
+                  placeholder="Unesi motivaciju za radionicu "
+                ></CustomTextArea>
+              </div>
+              <div className="drugiIzborDiv">
+                <label className="brojPitanja">14.</label>
+                <CustomSelect
+                  label="Odaberi alternativnu radionicu kojoj želiš da prisustvuješ"
+                  name="drugaRadionica"
+                  placeholder="sta je ovo"
+                >
+                  <option value="">
+                    Odaberi alternativnu radionicu kojoj želiš da prisustvuješ
+                  </option>
+                  <option value="Java">Java</option>
+                  <option value="Python">Python</option>
+                  <option value="React">React</option>
+                  <option value="Figma">Figma</option>
+                </CustomSelect>
+              </div>
+              <div className="drugaMotivacija">
+                <label className="brojPitanja">15.</label>
+                <CustomTextArea
+                  label="Šta te najviše interesuje u vezi oblasti koja se prelazi na ovoj radionici? "
+                  name="drugaMotivacija"
+                  placeholder="Zasto se prijavljujes za ovo "
+                ></CustomTextArea>
+              </div>
+              <button disabled={isSubmitting} type="submit">
+                Prijava
+              </button>
+            </Form>
+          )}
+        </Formik>
+        {/* {modalOpen ? (
         <div className="success-modal-container">
           <div className="success-modal-content">
             <p>{modalMessage}</p>
@@ -380,6 +411,7 @@ const Prijava = () => {
           </div>
         </div>
       ) : null} */}
+      </div>
     </Scrollbars>
   );
 };
