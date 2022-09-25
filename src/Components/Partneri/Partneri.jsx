3d612a7c17340aa1bb7f543ab36f0ea261b3abb7
VeljkoBlagojevic <veljkoblagojevic008@gmail.com>
2022-09-25 19:32:22 +0200

    godisnji logo i linkovi

--- src/Components/Partneri/Partneri.jsx
+++ src/Components/Partneri/Partneri.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "../Partneri/Partneri.css";
 import Slider from "react-slick";
 
-// import Adacta from "../../assets/adacta.png";
-import DragiBravo from "../../assets/partneriLogo/dragibravo.jpeg";
-import LogoPartner1 from "../../assets/partneriLogo/glassart.png";
-import LogoPartner2 from "../../assets/partneriLogo/beerpong.png";
-import LogoPartner3 from "../../assets/partneriLogo/danza.png";
-import LogoPartner4 from "../../assets/partneriLogo/drbrado.png";
-import LogoPartner5 from "../../assets/partneriLogo/futrola.png";
-import LogoPartner6 from "../../assets/partneriLogo/Gifty.png";
-import LogoPartner7 from "../../assets/partneriLogo/naucidizajn.jpg";
-import LogoPartner8 from "../../assets/partneriLogo/kolacic.png";
+import Adacta from "../../assets/adacta.png";
+import DragiBravo from "../../assets/dragibravo.jpeg";
+import SlikaTim1 from "../../assets/glassart.png";
+import SlikaTim2 from "../../assets/adacta.png";
+import SlikaTim3 from "../../assets/adacta.png";
+import SlikaTim4 from "../../assets/adacta.png";
+import SlikaTim5 from "../../assets/adacta.png";
+import SlikaTim6 from "../../assets/adacta.png";
+import SlikaTim7 from "../../assets/adacta.png";
+
+import DevIT from "../../assets/godisnji/devit.png";
+import Digitec from "../../assets/godisnji/digitec.png";
+import FON from "../../assets/godisnji/fon.png";
+import Guarana from "../../assets/godisnji/guarana.png";
+import Hedwell from "../../assets/godisnji/hedwell.png";
+import Mainstream from "../../assets/godisnji/mainstream.png";
+import Prime from "../../assets/godisnji/prime.png";
 
 import { motion } from "framer-motion";
 
@@ -44,83 +51,53 @@ const Partneri = () => {
 
   const godisnjiPartneri = [
     {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
+      logoPartnera: DevIT,
+      linkPartnera: "https://www.devit.rs/",
     },
     {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
+      logoPartnera: Digitec,
+      linkPartnera: "https://www.digitec.rs/reklamni_materijali",
     },
     {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
+      logoPartnera: FON,
+      linkPartnera: "http://fon.bg.ac.rs/",
     },
     {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
+      logoPartnera: Guarana,
+      linkPartnera: "https://knjaz.rs/portfolio/guarana/",
     },
     {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
+      logoPartnera: Hedwell,
+      linkPartnera: "https://hedwell.com/",
+    },
+    {
+      logoPartnera: Mainstream,
+      linkPartnera: "https://www.mainstream.rs/",
+    },
+    {
+      logoPartnera: Prime,
+      linkPartnera: "https://www.primeholding.com/",
     },
   ];
   const logoPartneri2 = [
-    {
-      logoPartnera: DragiBravo,
-      linkPartnera: "https://www.instagram.com/dragibravo/",
-    },
-    {
-      logoPartnera: LogoPartner1,
-      linkPartnera: "https://www.instagram.com/glassart.srbija/",
-    },
-    {
-      logoPartnera: LogoPartner2,
-      linkPartnera: "http://www.facebook.com/beerpongbarbelgrade/",
-    },
-    {
-      logoPartnera: LogoPartner3,
-      linkPartnera: "https://www.instagram.com/danzadelarte/?hl=en",
-    },
-    {
-      logoPartnera: LogoPartner4,
-      linkPartnera: "https://brados.rs/",
-    },
-    {
-      logoPartnera: LogoPartner5,
-      linkPartnera: "https://www.instagram.com/la_futrola/",
-    },
-    {
-      logoPartnera: LogoPartner6,
-      linkPartnera: "https://gifty.rs/",
-    },
-    {
-      logoPartnera: LogoPartner7,
-      linkPartnera: "https://www.instagram.com/naucidizajn/",
-    },
-    {
-      logoPartnera: LogoPartner8,
-      linkPartnera: "http://www.kolacicsrece.com/",
-    },
+    DragiBravo,
+    SlikaTim1,
+    SlikaTim2,
+    SlikaTim3,
+    SlikaTim4,
+    SlikaTim5,
+    SlikaTim6,
+    SlikaTim7,
   ];
-  // const logoPartneri2 = [
-  //   DragiBravo,
-  //   LogoPartner1,
-  //   LogoPartner2,
-  //   LogoPartner3,
-  //   LogoPartner4,
-  //   LogoPartner5,
-  //   LogoPartner6,
-  //   LogoPartner7,
-  // ];
   // const logoPartneri3 = [
   //   DragiBravo,
-  //   LogoPartner1,
-  //   LogoPartner2,
-  //   LogoPartner3,
-  //   LogoPartner4,
-  //   LogoPartner5,
-  //   LogoPartner6,
-  //   LogoPartner7,
+  //   SlikaTim1,
+  //   SlikaTim2,
+  //   SlikaTim3,
+  //   SlikaTim4,
+  //   SlikaTim5,
+  //   SlikaTim6,
+  //   SlikaTim7,
   // ];             OVI SU ZA MEDIJSKE
 
   return (
@@ -178,15 +155,13 @@ const Partneri = () => {
           <Slider {...settingsPartneri} className="SliderCeo">
             {logoPartneri2.map((img) => (
               <div className="Jedan-partner">
-                <a href={img.linkPartnera}>
                 <img
                   className="Jedan-partner-slika"
-                  src={img.logoPartnera}
+                  src={img}
                   alt={
                     "Naturalni i robni partneri projekta Studenti Studentima"
                   }
                 ></img>
-                </a>
               </div>
             ))}
           </Slider>
